5ae335fcf0c0f1be5d9d3935e058fa83f1778864
Daisiesz <beckybecklyn@gmail.com>
2025-08-11 02:06:05 +0330

    My assignment submission

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python_list
+My list submission as per the assignment.
+
+Has the code and comments to clearly state the flow of my code.
---
+++ list.py
@@ -0,0 +1,13 @@
+my_list =[]
+#Adding elements to a list
+my_list.append(10,20,30)
+#Extending a list
+my_list.extend([50,60,70])
+#Inserting a value at a specific position
+my_list.insert(1,15)
+#Removiing a value
+my_list.pop(6)
+#Sorting in ascending order
+my_list.sort()
+#Finding a specific element
+print(my_list[3])
