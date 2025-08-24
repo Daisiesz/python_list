40e297bdaf1a4e14158acce9387b529d64c939a3
Daisiesz <beckybecklyn@gmail.com>
2025-08-24 03:43:17 +0330

    Final attempt

--- File_Handling/file.py
+++ File_Handling/file.py
@@ -1,1 +1,20 @@
-File read & write
+#Creating a new file. 
+file = open("newdocument.txt", "w")
+
+# Writing to the file
+file.write("I'm learning Python.")
+
+# Reading the file
+file = open("newdocument.txt", "r")
+print(file.read())
+
+# Handling errors
+try:
+    file = open("newdocument.txt", "r")
+    print(file.read())
+except FileNotFoundError:
+    print("File not found.")
+except Exception as e:
+    print("Error:", e)
+finally:
+    file.close()
